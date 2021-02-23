d3a1274833a0c4d8cb8509c69da48097fb1e0d30
Владислав Секисов <sekisov@rosohrana.ru>
2021-02-23 19:24:35 +0300

    Add animation rabbit

---
+++ Animation/Assets/Script/ControllerRebbit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerRebbit : MonoBehaviour
+{
+	enum PlayerStatus
+	{
+		Walk, 
+		Rake,
+		Death
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			GetComponent<Animator>().SetInteger("PlayerStatus", 0);
+			//GetComponent<Animator>().SetBool("Rake", true);
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			GetComponent<Animator>().SetInteger("PlayerStatus", 1);
+			//GetComponent<Animator>().SetBool("Rake", false);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Animator>().SetInteger("PlayerStatus", 2);
+		}
+	}
+}
